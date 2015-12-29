38038a8e9d2f754314fcd629be5b45e07ef12111
Onisdel Echavarria C <onisdel85@gmail.com>
2015-12-29 13:54:08 -0500

    dialog

--- assets/lib/app/PersonalDataForm.js
+++ assets/lib/app/PersonalDataForm.js
@@ -1,5 +1,6 @@
 define([
     "dojo/_base/declare",
+    "dojo/_base/lang",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
@@ -7,6 +8,8 @@ define([
     "dojo/text!./templates/PersonalDataForm.html",
     "dojo/store/Memory",
     "dojo/on",
+    "dijit/Dialog",
+    "dojo/dom",
     "dojo/_base/config",
     "dijit/registry",
     "dojo/date/locale",
@@ -19,16 +22,15 @@ define([
     "dijit/form/ValidationTextBox",
     "dijit/form/DateTextBox",
     "dojox/validate/web"
-], function (declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin,
-             i18n, template, Memory, on, config, registry, locale, query) {
-
-
+], function (declare, lang, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin,
+             i18n, template, Memory, on,Dialog, dom, config, registry, locale, query, ValidationTextBox) {
     var genderStore = new Memory({
         data: [
             {name: i18n.genderMaleLabel, id: i18n.genderMaleLabel},
             {name: i18n.genderFemaleLabel, id: i18n.genderFemaleLabel}
         ]
     });
+    // Create a new instance of dijit/Dialog
 
     return declare('app.PersonalDataForm', [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
         Translate: i18n,
@@ -37,8 +39,23 @@ define([
         _setGenderStore: function () {
             var genderSelect = registry.byId('genderId');
             genderSelect.set('store', genderStore);
-        },
-
+        }
+        ,
+        _myDisplayMessage: lang.hitch(this, function (message) {
+            // You can do whatever you want with the message...
+            if (typeof message != 'undefined')
+                dom.byId("messageMail").innerHTML = message;
+        }),
+        _myDisplayMessageMail: lang.hitch(this, function (message) {
+            // You can do whatever you want with the message...
+            if (typeof message != 'undefined')
+                dom.byId("messageName").innerHTML = message;
+        }),
+        _myDisplayMessageSurname: lang.hitch(this, function (message) {
+            // You can do whatever you want with the message...
+            if (typeof message != 'undefined')
+                dom.byId("messageSurname").innerHTML = message;
+        }),
         _confirm: function () {
             var isValid = dataForm.validate();
             if (isValid && !dataForm.get('isconfirmed')) {
@@ -80,6 +97,7 @@ define([
             }
             return isValid;
         }
+
         ,
         _fixFormTitle: function () {
             if (config.locale == "es-es" || config.locale == "es")
@@ -96,14 +114,44 @@ define([
             registry.byId('continueBtn').set("disabled", true);
         }
         ,
+
         postCreate: function () {
             var self = this;
             var dataForm = registry.byId('dataForm');
             on(this._addBtn, 'click', this._confirm);
             on(this._backBtn, 'click', this._goBack);
+            on(this._upload, 'click', this._uploadTest);
+            on(this._diagBtn, 'click', this._showDialog);
+            on(this._diagCloseBtn, 'click', this._hideDialog);
+            //on(this._myDisplayMessage(), 'click', this.);
             this._fixFormTitle();
             this._setGenderStore();
             this.inherited(arguments);
+        }
+        ,
+        _uploadTest: function () {
+            alert();
+
+            dojoConfig.locale = "en";
+
+        }
+        ,
+        myDialog: new Dialog({
+            // The dialog's title
+            title: "The Dojo Toolkit",
+            // The dialog's content
+            content: "This is the dialog content.",
+            // Hard-code the dialog width
+            style: "width:200px;"
+        }),
+        _showDialog: function () {
+           // debbug;
+            alert();
+            registry.byId("terms").show();
+        },
+        // Hide the dialog
+        _hideDialog: function () {
+            registry.byId("terms").hide();
         }
     });
 
